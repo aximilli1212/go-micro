2eb3ee1c72f9a07335f430d6bd719337af55e667
Emmanuel Sackey <aximilli1212@gmail.com>
2023-07-21 03:08:47 +0000

    Add method to broker service to communicate with logger service and return a response

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -16,6 +16,12 @@ type AuthPayload struct {
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -40,10 +46,52 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
 
+	case "log":
+		app.logItem(w, requestPayload.Log)
+
 	default:
 		app.errorJSON(w, errors.New("unknown Action"), http.StatusBadRequest)
 	}
 
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	//create some json we'll send to the auth microservice
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	//call the service ---- USING HTTP CLIENT
+	request, err := http.NewRequest("POST", "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{} // create a client
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// make sure we get back the correct status code
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling auth service"))
+		log.Println(response.StatusCode)
+		log.Println(response)
+		log.Println(err)
+		log.Println("----------")
+		log.Println(bytes.NewBuffer(jsonData))
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Logged!"
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
