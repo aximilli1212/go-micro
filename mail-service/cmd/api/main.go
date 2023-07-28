a0ce61005b8a419895a800ff3dab466a12988dbe
Emmanuel Sackey <aximilli1212@gmail.com>
2023-07-28 03:27:34 +0000

    Add create mail function

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
@@ -25,4 +28,20 @@ func main() {
 		log.Println("Error starting mail server:", err)
 		log.Panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+	}
+
+	return m
 }
